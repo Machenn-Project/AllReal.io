f0180f4665c0d1186ec5d372f5f52521ed6a8f8a
PUVIYARASU P <puviyarasu5341@gmail.com>
2024-06-13 11:40:56 +0530

    company name

--- Editor/AllReal.cs
+++ Editor/AllReal.cs
@@ -1,15 +1,44 @@
-
+using UnityEditor;
 using UnityEngine;
 
-namespace AllReal.Editor 
+namespace AllReal
 {
-    
-    public class Test{
-
-        public void display(){
-            Debug.Log("Test");
+    [InitializeOnLoad]
+    public static class ChangeSettings
+    {
+        static ChangeSettings()
+        {
+            EditorApplication.update += OnEditorUpdate;
         }
 
+        private static void OnEditorUpdate()
+        {
+            // Ensure this runs only once
+            EditorApplication.update -= OnEditorUpdate;
+
+            // Set the default company name
+            SetCompanyName("AllReal");
+
+            // Set the default build platform
+            //SetBuildPlatform(BuildTarget.StandaloneWindows64);
+        }
+
+        private static void SetCompanyName(string companyName)
+        {
+            if (PlayerSettings.companyName != companyName)
+            {
+                PlayerSettings.companyName = companyName;
+                Debug.Log("Company name set to: " + companyName);
+            }
+        }
+
+        private static void SetBuildPlatform(BuildTarget target)
+        {
+            if (EditorUserBuildSettings.activeBuildTarget != target)
+            {
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, target);
+                Debug.Log("Build platform set to: " + target.ToString());
+            }
+        }
     }
 }
-
